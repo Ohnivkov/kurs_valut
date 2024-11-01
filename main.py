489d559169dc0b9f3126d0daec7c5c91bb8b122a
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2024-11-01 12:40:08 +0200

    New structure

--- main.py
+++ main.py
@@ -1,68 +1,135 @@
+import json
+import os
+
 import requests
+import telebot
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
-import json
-import telebot
-import os
 
 user_file = os.path.join(os.getcwd(), 'users.json')
 curs_file = os.path.join(os.getcwd(), 'kurs.json')
 
 
-def privatbank(a):
-    r = requests.get('https://privatbank.ua/', timeout=10)
-    get_bs_obj = bs(r.text, 'html.parser')
-    return float(get_bs_obj.find('td', id=a).text.strip())
+class PrivatBank:
+    def get_kurs_dollarsell(self):
+        r = requests.get('https://privatbank.ua/', timeout=10)
+        get_bs_obj = bs(r.text, 'html.parser')
+        return float(get_bs_obj.find('td', id='USD_sell').text.strip())
+
+    def get_kurs_dollarbuy(self):
+        r = requests.get('https://privatbank.ua/', timeout=10)
+        get_bs_obj = bs(r.text, 'html.parser')
+        return float(get_bs_obj.find('td', id='USD_buy').text.strip())
 
 
-def ukrsib():
-    ua = UserAgent()
-    kurs_dict = {}
-    header = {'User-Agent': ua.chrome}
-    r = requests.get("https://ukrsibbank.com/currency-cash/", headers=header)
-    get_bs_obj = bs(r.text, 'html.parser')
-    values = get_bs_obj.findAll('div', class_='exchange__item-text')
-    kurs_dict['USD'] = {"sell": float(values[2].text.strip().replace(',', '.')),
-                        'buy': float(values[1].text.strip().replace(',', '.'))}
-    kurs_dict['EUR'] = {"sell": float(values[6].text.strip().replace(',', '.')),
-                        'buy': float(values[5].text.strip().replace(',', '.'))}
-    return kurs_dict
+    def get_kurs_eurosell(self):
+        r = requests.get('https://privatbank.ua/', timeout=10)
+        get_bs_obj = bs(r.text, 'html.parser')
+        return float(get_bs_obj.find('td', id='EUR_sell').text.strip())
+
+    def get_kurs_eurobuy(self):
+        r = requests.get('https://privatbank.ua/', timeout=10)
+        get_bs_obj = bs(r.text, 'html.parser')
+        return float(get_bs_obj.find('td', id='EUR_buy').text.strip())
 
 
-def oshadbank():
-    l = {}
-    res = {}
-    r = requests.get("https://www.oschadbank.ua")
-    get_bs_obj = bs(r.text, 'html.parser')
-    for line in get_bs_obj.findAll('div', class_='currency__item', limit=2):
-        buy, sell = line.findAll('span', class_='currency__item_value')
-        if line.find('span', class_="currency__item_name").text == 'USD':
-            res['usd_sell'] = sell.span.text
-            res['usd_buy'] = buy.span.text
-        if line.find('span', class_="currency__item_name").text == 'EUR':
-            res['eur_sell'] = sell.span.text
-            res['eur_buy'] = buy.span.text
-            l = {'USD': {'sell': float(res['usd_sell']), 'buy': float(res['usd_buy'])},
-                 'EUR': {'sell': float(res['eur_sell']), 'buy': float(res['eur_buy'])}}
-    return l
+class UkrSib:
+    def get_kurs_dollarsell(self):
+        ua = UserAgent()
+        header = {'User-Agent': ua.chrome}
+        r = requests.get("https://ukrsibbank.com/currency-cash/", headers=header)
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.find_all('div', class_='module-exchange__item-text')
+        return float(values[1].text)
+
+    def get_kurs_dollarbuy(self):
+        ua = UserAgent()
+        header = {'User-Agent': ua.chrome}
+        r = requests.get("https://ukrsibbank.com/currency-cash/", headers=header)
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('div', class_='module-exchange__item-text')
+        return float(values[3].text)
+
+    def get_kurs_eurosell(self):
+        ua = UserAgent()
+        header = {'User-Agent': ua.chrome}
+        r = requests.get("https://ukrsibbank.com/currency-cash/", headers=header)
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('div', class_='module-exchange__item-text')
+        return float(values[7].text.strip().replace(',', '.'))
+
+    def get_kurs_eurobuy(self):
+        ua = UserAgent()
+        header = {'User-Agent': ua.chrome}
+        r = requests.get("https://ukrsibbank.com/currency-cash/", headers=header)
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('div', class_='module-exchange__item-text')
+        return float(values[9].text.strip().replace(',', '.'))
 
 
-def kredobank():
-    r = requests.get("https://kredobank.com.ua/info/kursy-valyut/commercial")
-    get_bs_obj = bs(r.text, 'html.parser')
-    res = {}
-    l = []
-    for line in get_bs_obj.findAll('div', class_='chart__number'):
-        l.append(line.text)
-    res['usd_sell'] = l[1]
-    res['usd_buy'] = l[0]
-    for line in get_bs_obj.findAll('td', limit=10):
-        l.append(line.text)
-    res['eur_sell'] = l[10]
-    res['eur_buy'] = l[11]
-    l1 = {'USD': {'sell': float(res['usd_sell']), 'buy': float(res['usd_buy'])},
-          'EUR': {'sell': float(res['eur_sell']), 'buy': float(res['eur_buy'])}}
-    return l1
+class OshadBank:
+    def get_kurs_dollarsell(self):
+        res = {}
+        r = requests.get("https://www.oschadbank.ua")
+        get_bs_obj = bs(r.text, 'html.parser')
+        for line in get_bs_obj.findAll('div', class_='currency__item', limit=2):
+            buy, sell = line.findAll('span', class_='currency__item_value')
+            if line.find('span', class_="currency__item_name").text == 'USD':
+                res['usd_sell'] = sell.span.text
+        return float(res['usd_sell'])
+    def get_kurs_dollarbuy(self):
+        res = {}
+        r = requests.get("https://www.oschadbank.ua")
+        get_bs_obj = bs(r.text, 'html.parser')
+        for line in get_bs_obj.findAll('div', class_='currency__item', limit=2):
+            buy, sell = line.findAll('span', class_='currency__item_value')
+            if line.find('span', class_="currency__item_name").text == 'USD':
+                res['usd_buy'] = sell.span.text
+
+        return float(res['usd_buy'])
+    def get_kurs_eurosell(self):
+        res = {}
+        r = requests.get("https://www.oschadbank.ua")
+        get_bs_obj = bs(r.text, 'html.parser')
+        for line in get_bs_obj.findAll('div', class_='currency__item', limit=2):
+            buy, sell = line.findAll('span', class_='currency__item_value')
+            if line.find('span', class_="currency__item_name").text == 'EUR':
+                res['eur_sell'] = sell.span.text
+
+        return float(res['eur_sell'])
+    def get_kurs_eurobuy(self):
+        res = {}
+        r = requests.get("https://www.oschadbank.ua")
+        get_bs_obj = bs(r.text, 'html.parser')
+        for line in get_bs_obj.findAll('div', class_='currency__item', limit=2):
+            buy, sell = line.findAll('span', class_='currency__item_value')
+            if line.find('span', class_="currency__item_name").text == 'EUR':
+                res['eur_buy'] = sell.span.text
+
+        return float(res['eur_buy'])
+class KredoBank:
+    def get_kurs_dollarsell(self):
+        r = requests.get("https://kredobank.com.ua/info/kursy-valyut/commercial")
+        get_bs_obj = bs(r.text, 'html.parser')
+        values=get_bs_obj.findAll('td')
+
+        return values[2].text
+    def get_kurs_dollarbuy(self):
+        r = requests.get("https://kredobank.com.ua/info/kursy-valyut/commercial")
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('td')
+        return values[3].text
+    def get_kurs_eurosell(self):
+        r = requests.get("https://kredobank.com.ua/info/kursy-valyut/commercial")
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('td')
+
+        return values[8].text
+    def get_kurs_eurobuy(self):
+        r = requests.get("https://kredobank.com.ua/info/kursy-valyut/commercial")
+        get_bs_obj = bs(r.text, 'html.parser')
+        values = get_bs_obj.findAll('td')
+        return values[9].text
 
 
 def getfilekurs():
@@ -112,13 +179,22 @@ def kirilitsia(bankname):
 
 def get_new_kurs():
     kurs = {}
-
+    privat = PrivatBank()
+    ukrsib = UkrSib()
+    oshad = OshadBank()
+    kredobank = KredoBank()
     kurs['privat'] = {
-        'USD': {"sell": privatbank("USD_sell"), "buy": privatbank("USD_buy")},
-        'EUR': {"sell": privatbank("EUR_sell"), "buy": privatbank("EUR_buy")}}
-    kurs['ukrsib'] = ukrsib()
-    kurs['oshad'] = oshadbank()
-    kurs['kredobank'] = kredobank()
+        'USD': {"sell": privat.get_kurs_dollarsell(), "buy": privat.get_kurs_dollarbuy()},
+        'EUR': {"sell": privat.get_kurs_eurosell(), "buy": privat.get_kurs_eurobuy()}}
+    kurs['ukrsib'] = {
+        'USD': {"sell": ukrsib.get_kurs_dollarsell(), "buy": ukrsib.get_kurs_dollarbuy()},
+        'EUR': {"sell": ukrsib.get_kurs_eurosell(), "buy": ukrsib.get_kurs_eurobuy()}}
+    kurs['oshad'] = {
+        'USD': {"sell": oshad.get_kurs_dollarsell(), "buy": oshad.get_kurs_dollarbuy()},
+        'EUR': {"sell": oshad.get_kurs_eurosell(), "buy": oshad.get_kurs_eurobuy()}}
+    kurs['kredobank'] = {
+        'USD': {"sell": kredobank.get_kurs_dollarsell(), "buy": kredobank.get_kurs_dollarbuy()},
+        'EUR': {"sell": kredobank.get_kurs_eurosell(), "buy": kredobank.get_kurs_eurobuy()}}
     return kurs
 
 
